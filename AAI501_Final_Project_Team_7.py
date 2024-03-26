8c35abe15e3010c5e4cce1c24d76eb6a2c6f0e7c
Matt Purkeypile <mpurkeypile@sandiego.edu>
2024-03-26 13:31:22 -0700

    Reading in of the local data file

--- AAI501_Final_Project_Team_7.py
+++ AAI501_Final_Project_Team_7.py
@@ -1,5 +1,15 @@
-# AAI501 Final Project, Team 7
+﻿# AAI501 Final Project, Team 7
 # Issa Ennab, Matt Purkeypile
 # University of San Diego
 # Spring 2024
 
+import pandas as pd
+import seaborn as sb
+import numpy as nump
+import statsmodels.formula.api as sm
+import matplotlib.pyplot as plot
+
+# pull the data locally
+AllData = pd.read_csv(".\\drugsComTrain_raw.tsv", sep = "\t")
+print(str(AllData.head()))
+print(str(AllData.shape))
